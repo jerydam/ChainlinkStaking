4a3c3318d95a18d37f0b59af6f0a1d0347f35349
Jerydam <jerydan148@gmail.com>
2023-02-22 15:53:41 +0100

    ss

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,9 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
-
   networks: {
     hardhat: {
+      forking: {
+        enabled: true,
+        url: "https://eth-mainnet.g.alchemy.com/v2/_h7b0uQquHnoOk3x_Bc8ciWU9q4uzEsc",
+      },
     },
     goerli: {
       url: process.env.GOERLI_RPC,
@@ -17,4 +20,5 @@ const config: HardhatUserConfig = {
     apiKey: process.env.API_KEY,
   }
 };
+
 export default config;
--- scripts/ChainSwap.ts
+++ scripts/ChainSwap.ts
@@ -1,13 +1,16 @@
-const { ethers } = require("hardhat");
 
-const TOKENA_ADDRESS = "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d";
-const TOKENB_ADDRESS = "0x0000000000000000000000000000000000000000";
-const PRICE_FEED_ADDRESS = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419";
-const AMOUNT = ethers.utils.parseEther("1");
+import { ethers } from "hardhat";
+import { BigNumber } from "ethers";
+import { providers } from "ethers";
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
 
+  const TOKENA_ADDRESS = "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d";
+const TOKENB_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const USDHolder = "0x748dE14197922c4Ae258c7939C7739f3ff1db573"
+const PRICE_FEED_ADDRESS = "0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9";
+const AMOUNT = ethers.utils.parseEther("1");
+console.log(AMOUNT);
   console.log("Deploying TokenSwap contract...");
   const TokenSwap = await ethers.getContractFactory("TokenSwap");
   const tokenSwap = await TokenSwap.deploy(TOKENA_ADDRESS, TOKENB_ADDRESS, PRICE_FEED_ADDRESS);
@@ -18,17 +21,20 @@ async function main() {
   const token1 = await ethers.getContractAt("IERC20", TOKENA_ADDRESS);
   const token2 = await ethers.getContractAt("IERC20", TOKENB_ADDRESS);
 
-  console.log(`Token1 balance before swap: ${await token1.balanceOf(deployer.address)}`);
-  console.log(`Token2 balance before swap: ${await token2.balanceOf(deployer.address)}`);
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
+await helpers.impersonateAccount(USDHolder);
+const impersonatedSigner = await ethers.getSigner(USDHolder);
+const USDContract = await ethers.getContractAt("IUSD", TOKENA_ADDRESS);
+const DaiContract = await ethers.getContractAt("IUSD",TOKENA_ADDRESS);
 
-  console.log("Swapping tokens...");
-  await token1.approve(tokenSwap.address, AMOUNT);
-  await tokenSwap.swapTokens(AMOUNT);
+const holderBalance = await USDContract.balanceOf(USDHolder);
+console.log(`USD balance before ${holderBalance}`);
+const getExchangeRate = await tokenSwap.connect(impersonatedSigner).getExchangeRate();
+const SwapToken = await tokenSwap.connect(impersonatedSigner).swapTokens(
+  AMOUNT,
 
-  console.log(`Token1 balance after swap: ${await token1.balanceOf(deployer.address)}`);
-  console.log(`Token2 balance after swap: ${await token2.balanceOf(deployer.address)}`);
+)
 }
-
 main()
   .then(() => process.exit(0))
   .catch(error => {
